109dd5c0a82be9fbd8db437de73910f3940d29d2
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-10-30 10:34:24 -0700

    gatekeeper

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
-
+const morgan = require('morgan');
 const server = express();
 
-server.use(helmet());
-server.use(express.json());
-server.use(logger);
-server.use('/api/hubs', hubsRouter);
-
+//function
 function logger(req, res, next) {
    const time = new Date().toLocaleTimeString();
    const date = new Date().toLocaleDateString();
@@ -17,6 +13,25 @@ function logger(req, res, next) {
    );
    next();
 }
+function gateKeeper(req, res, next) {
+   // data can come in the body, url parameters, query string, headers
+   // new way of reading data sent by the client
+   const password = req.headers.password || '';
+
+   if (password.toLowerCase() === 'mellon') {
+      next();
+   } else {
+      res.status(401).json({ message: 'enter the password.' });
+   }
+}
+
+//global middleWare
+server.use('/api/hubs', hubsRouter);
+server.use(helmet());
+server.use(express.json());
+server.use(logger);
+server.use(morgan('dev'));
+server.use(gateKeeper);
 
 server.get('/', (req, res) => {
    const nameInsert = req.name ? ` ${req.name}` : '';
